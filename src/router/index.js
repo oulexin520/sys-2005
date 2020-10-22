630414f6ae2c19f88502841d34035e9110f71a05
oulexin520 <@LAPTOP-4CIER583>
2020-10-22 10:53:08 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../Pages/Login";
-import allroutes from "./allRoutes";
+// import allroutes from "./allRoutes";
 
 Vue.use(VueRouter);
 
